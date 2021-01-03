201cca5d49b61ef6c05fe4d05d4dfd6b8c271a82
Jacob Gilhaus <gilhaus@wustl.edu>
2021-01-03 13:46:31 -0600

    lat/lng data packed into encrypted file

--- JG_spd.py
+++ JG_spd.py
@@ -1,12 +1,15 @@
 import os, random, struct
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
 import getpass
 import time
 import pwd
 import _thread
 import hmac
 import hashlib
+import geocoder
 
 # File supplied to and adapted by Jacob Gilhaus for a final
 # project in CSE433 at Washington University in St. Louis with
@@ -36,16 +39,51 @@ except ImportError:
         return padded_data[:-padding_len]
 
 # define a function to encrypt the files according to the key supplied
-def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
+def encrypt_file(key, in_filename, out_filename=None, file_anchor=None, allowed_distance=50, chunksize=64*1024):    
     # just add .enc if no new filename supplied
     if not out_filename:
         out_filename = in_filename + '.enc'
 
+    if not file_anchor:
+        file_anchor = '3312 Eagle Crest Rd Bloomington, Illinois'
+
     # get some needed values to encrypt the file
-    # use AES in CBC mode with random iv
+    # use AES in CBC mode with random iv and setup longitude and latitude
     iv = os.urandom(16)
     encryptor = AES.new(key, AES.MODE_CBC, iv)
     filesize = os.path.getsize(in_filename)
+    print(file_anchor)
+    file_anchor = geolocator.geocode(file_anchor)
+    print(file_anchor)
+    anchlat = float(file_anchor.latitude)
+    anchlng = float(file_anchor.longitude)
+    
+    # must be 8 characters to maintain offset
+    if anchlat < 0:
+        if anchlat > -100:
+            anchlat = float("%07.4f"%anchlat)
+        else:
+            anchlat = float("%07.3f"%anchlat)
+    else:
+        if anchlat < 100:
+            anchlat = float("%07.5f"%anchlat)
+        else:
+            anchlat = float("%07.4f"%anchlat)
+
+    
+    if anchlng < 0:
+        if anchlng > -100:
+            anchlng = float("%07.4f"%anchlng)
+        else:
+            anchlng = float("%07.3f"%anchlng)
+    else:
+        if anchlng < 100:
+            anchlng = float("%07.5f"%anchlng)
+        else:
+            anchlng = float("%07.4f"%anchlng)
+
+    print(anchlng, anchlat)
+    print(len(struct.pack('<f', anchlng)), len(struct.pack('<f', anchlat)))
 
     # write the encrypted file with filesize and iv first
     # encrypt chunks at a time at the end of the write loop
@@ -53,6 +91,10 @@ def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
         with open(out_filename, 'wb') as outfile:
             outfile.write(struct.pack('<Q', filesize))
             outfile.write(iv)
+
+            # TODO move the lat and lng to be encrypted?
+            outfile.write(struct.pack('<f', anchlng))
+            outfile.write(struct.pack('<f', anchlat))
             pos = 0
             while pos < filesize:
                 chunk = infile.read(chunksize)
@@ -65,6 +107,17 @@ def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
 
 # reverse the encrypt function, requires correct key or may throw error
 def decrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
+    # check location parameters before allowing access
+    # TODO update dummy values here!
+    latdist = loc.lat - 40
+    lngdist = loc.lng + 89
+
+    if latdist > 1 or latdist < -1 or lngdist > 1 or lngdist < -1:
+        print('Distance Violation!')
+        print(latdist)
+        print(lngdist)
+        return
+
     # if no new filename, add .dec
     if not out_filename:
         out_filename = in_filename + '.dec'
@@ -74,11 +127,13 @@ def decrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
         # take the filesize and iv first
         filesize = struct.unpack('<Q', infile.read(8))[0]
         iv = infile.read(16)
+        filelat = struct.unpack('<f', infile.read(4))
+        filelng = struct.unpack('<f', infile.read(4))
         encryptor = AES.new(key, AES.MODE_CBC, iv)
         with open(out_filename, 'wb') as outfile:
             encrypted_filesize = os.path.getsize(in_filename)
-            # the filesize and IV
-            pos = 8 + 16
+            # the filesize, IV, lng, and lat metadata
+            pos = 8 + 16 + 4 + 4
             while pos < encrypted_filesize:
                 chunk = infile.read(chunksize)
                 pos += len(chunk)
@@ -122,6 +177,14 @@ if __name__=='__main__':
     os.system('rm -rf ./op_trace.txt')
     os.system('rm -rf ./op_monitor.txt')
 
+    # get users location for geosense and geofence
+    loc = geocoder.ip('me')
+    print(loc)
+    print(loc.latlng)
+
+    # setup geopy for calculating distance from anchor point\
+    geolocator = Nominatim(user_agent='JG_SPD')
+
     # get the user's key, must be same as encrypted key to decrypt
     passwd = getpass.getpass("input your key:\n")
     hash_pwd = SHA256.new()
@@ -139,15 +202,18 @@ if __name__=='__main__':
         elif op == '1':
             # encrypt, give prompts for optional filenames and call encrypt
             input_path = input("Input plaintext file path:\n")
-            output_path = input("Input encrypted file path\n")
+            output_path = input("Input encrypted file path:\n")
+            file_anchor = input("Input file anchor address:\n")
+            allowed_distance = input("Input allowed distance from anchor point in miles:\n")
+
             data_op_record('encrypt')
-            encrypt_file(hash_pwd.hexdigest()[0:32].encode('utf-8'),input_path,output_path)
+            encrypt_file(hash_pwd.hexdigest()[0:32].encode('utf-8'), input_path, output_path, file_anchor, allowed_distance)
         elif op == '2':
             # decrypt, give prompts for optional filenames and call decrypt
             input_path = input("Input encrypted file path:\n")
-            output_path = input("Input plaintext file path\n")
+            output_path = input("Input plaintext file path:\n")
             data_op_record('decrypt')
-            decrypt_file(hash_pwd.hexdigest()[0:32].encode('utf-8'),input_path,output_path)
+            decrypt_file(hash_pwd.hexdigest()[0:32].encode('utf-8'), input_path, output_path)
         elif op == '3':
             # user wants to remove a file, ask which one and delete
             input_path = input("Input path of the file you want to delete:\n")
